40c7fcef57adb214896e6b943869b68dccf9a468
Iraklu <chaxva32@gmail.com>
2023-08-17 05:02:31 -0700

    :fire: updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='mx-auto flex max-w-2xl flex-col gap-4 py-12'>
+    <div className='mx-auto flex max-w-2xl flex-col gap-4 py-12 overflow-x-hidden'>
       <PostCard/>
     </div>
   )
--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -4,8 +4,8 @@ import { useState, useEffect } from 'react'
 import { getDocs, collection } from 'firebase/firestore'
 import { db } from '@/lib/fiebase/page'
 import { Post } from '@/types/post'
-import { format } from 'date-fns';
-
+import { IconShare } from '@tabler/icons-react'
+ 
 const PostCard = () => {
     const [postLists, setPostLists] = useState<Post[]>();;
     const postCollectionRef = collection(db, "posts");
@@ -20,14 +20,25 @@ const PostCard = () => {
         getPosts();
     }, []);
 
-  return <div className='flex justify-center items-center mt-10'> 
+  return <div  className='flex flex-col gap-4 justify-center items-center mt-10 w-full'> 
     {postLists?.map((post)=> {
-        return <div key={post.id} className='text-2xl'>{post.title}
-            <img src={post.author?.pfp} alt="" />  
-            <div>
-            {post.createdAt}
+        return <div key={post.id} className='flex flex-col gap-1.5 w-full'>
+            <div className='px-2 flex flex-col w-full'>
+                <div className='rounded-md border px-4 py-2'>
+                    <h1 className='text-xl py-2'>{post.title}</h1>
+                    <hr className='mt-2 mb-4'/>
+                        <div className='flex justify-between items-center'>
+                            <div className='flex justify-center items-center'>
+                                <img className='h-8 rounded-3xl' src={post.author?.pfp}/> 
+                                <span className='px-2 text-sm font-bsemibold'> - {post.createdAt}</span>
+                            </div>
+                            <div className='flex justify-end items-center'>
+                                <IconShare/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
     })} 
   </div>
 }
