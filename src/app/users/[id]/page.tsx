179f57915547e22cc217502f52b97a4682305bdd
Iraklu <chaxva32@gmail.com>
2023-08-21 04:37:39 -0700

    upadtesss

--- src/app/users/[id]/page.tsx
+++ src/app/users/[id]/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from 'react';
 import { auth, db } from '@/lib/firebase/page';
 import { doc, getDoc, collection, query, where, getDocs } from 'firebase/firestore';
+import Link from 'next/link';
 
 type User = {
   displayName: string;
@@ -27,6 +28,7 @@ type Post = {
 const UserProfilePage = () => {
   const [user, setUser] = useState<User | null>(null);
   const [userPosts, setUserPosts] = useState<Post[]>([]);
+  
 
   useEffect(() => {
     const getUserData = async () => {
@@ -42,9 +44,10 @@ const UserProfilePage = () => {
           // Retrieve user's posts
           const postsQuery = query(collection(db, 'posts'), where('author.id', '==', currentUser.uid));
           const querySnapshot = await getDocs(postsQuery);
-          const posts = querySnapshot.docs.map((doc) => doc.data() as Post);
+          const posts = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() } as Post));
           setUserPosts(posts);
-          console.log(setUserPosts(posts));
+          console.log(setUserPosts(posts));    
+
         }
       }
     };
@@ -55,30 +58,31 @@ const UserProfilePage = () => {
   return (
     <div className='mx-auto min-h-[calc(100vh-68px)] max-w-4xl px-8 pb-16 pt-24'>
       {user ? (
-        <div className='flex items-center p-2'>
+        <div className='flex flex-col items-start p-2'>
           <div className='flex items-center'>
             <img className='rounded-full' src={user.photoUrl} alt="" />
             <h1 className='font-extrabold text-2xl px-4'>{user.displayName}</h1>
           </div>
+          <hr className='my-4 border-gray-500/80 w-full' />
         </div>
       ) : (
         <p>Loading...</p>
       )}
 
-      {userPosts.length > 0 && (
+      {userPosts.length > 0 ? (
         <div>
-          <h2 className='font-semibold text-lg'>Posts by {user?.displayName}</h2>
-          <ul>
             {userPosts.map((post) => (
-              <li key={post.id}>
-                <div>
-                  <h3>{post.title}</h3>
-                  <p>{post.postText}</p>
-                  {/* You can display other post details here */}
+              <div key={post.id}>
+                <h1>Posts by {user?.displayName} : </h1>
+                <div className='flex flex-col p-4'>
+                  <Link href={`/posts/${post.id}`}><h3 className='text-2xl'>{post.title}</h3></Link> 
                 </div>
-              </li>
+              </div>
             ))}
-          </ul>
+        </div>
+      ) : (
+        <div>
+              
         </div>
       )}
     </div>
